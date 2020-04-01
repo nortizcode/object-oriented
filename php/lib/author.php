a8dbc0b20f7699b1f12a44bc0a3b0a93f9d70f79
Nathan Ortiz <nkortiz92@gmail.com>
2020-04-01 11:53:17 -0600

    finished instantiating my object

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,17 +1,18 @@
 <?php
 
 require_once(dirname(__DIR__) . "/Classes/autoload.php");
-require_once \Nortiz41\ObjectOriented\Author::
+require_once(dirname(__DIR__, 1) . "/Classes/Author.php");
+use \Nortiz41\ObjectOriented\Author;
 
 
 	$authorId= "7214d724-3f4e-4691-bd3d-9540adb91b14";
 	$authorActivationToken= bin2hex(random_bytes(16));
 	$authorEmail= "nkortiz92@gmail.com";
-	$authorHash= "password";
+	$authorHash= password_hash("password", PASSWORD_ARGON2I, ["time_cost" => 45]);
 	$authorAvatarUrl= "https://avatar.com";
 	$authorUsername= "nathan-ortiz";
 
 
-$author = new \Nortiz41\ObjectOriented\Author($authorId, $authorActivationToken,$authorEmail, $authorHash, $authorAvatarUrl, $authorUsername);
+$author = new Author($authorId, $authorActivationToken,$authorEmail, $authorHash, $authorAvatarUrl, $authorUsername);
 
 	var_dump($author);
