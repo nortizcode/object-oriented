01ed70c551145e957d2c677e1e89aff4167691aa
Nathan Ortiz <nkortiz92@gmail.com>
2020-04-09 14:23:35 -0600

    made uuid.php

---
+++ php/Classes/Test/AuthorTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Nortizcode\DataDesign\Test;
+
+use Nortizcode\ObjectOriented\{Author};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+
+class AuthorTest extends DataDesignTest {
+
+
+
+}
