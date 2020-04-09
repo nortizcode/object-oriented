c8f1e99f77974910d772503e0f5aac04dcc0016b
Nathan Ortiz <nkortiz92@gmail.com>
2020-04-09 15:33:14 -0600

    made state variables AuthorTest.php

--- php/Classes/Test/AuthorTest.php
+++ php/Classes/Test/AuthorTest.php
@@ -15,6 +15,34 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 class AuthorTest extends DataDesignTest {
 
+private $VALID_AUTHOR_ACTIVATION_TOKEN; //done in set up
+private $VALID_AUTHOR_EMAIL = "nkortiz92@gmail.com";
+private $VALID_AUTHOR_HASH; = //done in set up
+private $VALID_AUTHOR_AVATAR = "https://avatar.com";
+private $VALID_AUTHOR_USERNAME= "Nortiz41";
 
+public function setUp(): void {
+	parent::setUp();
+
+	$password = "my_secret_password";
+	$this->VALID_AUTHOR_HASH = password_hash($password, PASSWORD_ARGON2ID, ["time_cost" => 45]);
+	$this->VALID_AUTHOR_ACTIVATION_TOKEN = bin2hex(random_bytes(16));
+}
+
+	public function testInsertValidAuthor() : void {
+
+}
+
+public function testDeleteValidAuthor() : void {
+
+}
+
+public function testGetValidAuthorByAuthorId() : void {
+
+}
+
+public function testGetValidAuthors() : void {
+
+}
 
 }
